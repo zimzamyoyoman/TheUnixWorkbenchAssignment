dec9c740f1323186dca48bef088c25bec9b45faf
Samarth Goyal <samarth_niku@yahoo.co.in>
2019-03-17 23:40:01 +0530

    Completed the entire program

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,2 +1,24 @@
-numfiles= ls -l | wc -l
-echo $numfiles
+function guessinggame { 
+	local numfiles=$(ls -l | wc -l)
+	local userguess=-1
+	
+	while [[ $userguess -ne $numfiles ]]
+	do
+		echo "Guess the number of files."
+		read userguess
+		
+		if [[ $userguess -gt $numfiles ]]
+		then 
+			echo "You guessed too high!"
+		elif [[ $userguess -lt $numfiles ]]
+		then 
+			echo "You guessed too low!"
+		fi 
+	done 
+
+	echo "You guessed correctly! Congratulations!"
+
+}
+
+guessinggame
+	
